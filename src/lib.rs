82a694a1c3f58f6313eca10d8d2363b36d9fc7ca
confuseSUN <778398626@qq.com>
2024-07-31 15:01:43 +0800

    read zkey

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,32 @@
 use anyhow::{anyhow, Error};
 use ark_bn254::{Bn254, Fr};
-use ark_circom::{CircomBuilder, CircomConfig};
-use ark_groth16::{Groth16, Proof};
+use ark_circom::{CircomBuilder, CircomConfig, CircomReduction};
+use ark_groth16::{Groth16, Proof, ProvingKey};
 use ark_snark::SNARK;
 use num_bigint::BigInt;
 use once_cell::sync::OnceCell;
 use rand::thread_rng;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File};
 
-type GrothBn = Groth16<Bn254>;
+type GrothBn = Groth16<Bn254, CircomReduction>;
 
 pub struct Input {
     pub maps: HashMap<String, Vec<BigInt>>,
 }
 
-static CONFIG: OnceCell<CircomConfig<Bn254>> = OnceCell::new();
+static CONFIG: OnceCell<(CircomConfig<Bn254>,ProvingKey<Bn254>)> = OnceCell::new();
 
-pub fn init_config(wasm: &str, r1cs: &str) {
+pub fn init_config(wasm: &str, r1cs: &str, zkey:&str) {
     let cfg = CircomConfig::<Bn254>::new(wasm, r1cs).unwrap();
-    CONFIG.set(cfg).unwrap();
+
+    let mut zkey_file = File::open(zkey).unwrap();
+    let (prover_key,_) =  ark_circom::read_zkey(&mut zkey_file).unwrap();
+
+    CONFIG.set((cfg,prover_key)).unwrap();
 }
 
 pub fn prove<T: TryInto<Input, Error = Error>>(input: T) -> Result<(Vec<Fr>, Proof<Bn254>), Error> {
-    let cfg = CONFIG
+    let (cfg,prover_key) = CONFIG
         .get()
         .ok_or_else(|| anyhow!("Failed to get circom config"))?;
 
@@ -30,17 +34,13 @@ pub fn prove<T: TryInto<Input, Error = Error>>(input: T) -> Result<(Vec<Fr>, Pro
     let builder_input = input.try_into()?;
     builder.push_inputs(builder_input.maps);
 
-    // TODO: setup
-    let circom = builder.setup();
-    let mut rng = thread_rng();
-    let params = GrothBn::generate_random_parameters_with_reduction(circom, &mut rng)?;
-
     let circom = builder.build().map_err(|_| anyhow!("Failed to build"))?;
     let pi = circom
         .get_public_inputs()
         .ok_or_else(|| anyhow!("Failed to get public inputs"))?;
 
-    let proof = GrothBn::prove(&params, circom, &mut rng)?;
+    let mut rng = thread_rng();
+    let proof = GrothBn::prove(prover_key, circom, &mut rng)?;
 
     Ok((pi, proof))
 }
