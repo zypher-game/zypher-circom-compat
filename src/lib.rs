8ed401da5dbaf78abd943ea1912b0b97ae61b46a
confuseSUN <778398626@qq.com>
2024-08-09 17:08:14 +0800

    switch rng

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ use ark_groth16::{Groth16, Proof, ProvingKey};
 use ark_snark::SNARK;
 use num_bigint::BigInt;
 use once_cell::sync::OnceCell;
-use rand::thread_rng;
+use rand::SeedableRng;
+use rand_chacha::ChaChaRng;
 use std::{collections::HashMap, fs::File};
 
 type GrothBn = Groth16<Bn254, CircomReduction>;
@@ -14,19 +15,19 @@ pub struct Input {
     pub maps: HashMap<String, Vec<BigInt>>,
 }
 
-static CONFIG: OnceCell<(CircomConfig<Bn254>,ProvingKey<Bn254>)> = OnceCell::new();
+static CONFIG: OnceCell<(CircomConfig<Bn254>, ProvingKey<Bn254>)> = OnceCell::new();
 
-pub fn init_config(wasm: &str, r1cs: &str, zkey:&str) {
+pub fn init_config(wasm: &str, r1cs: &str, zkey: &str) {
     let cfg = CircomConfig::<Bn254>::new(wasm, r1cs).unwrap();
 
     let mut zkey_file = File::open(zkey).unwrap();
-    let (prover_key,_) =  ark_circom::read_zkey(&mut zkey_file).unwrap();
+    let (prover_key, _) = ark_circom::read_zkey(&mut zkey_file).unwrap();
 
-    CONFIG.set((cfg,prover_key)).unwrap();
+    CONFIG.set((cfg, prover_key)).unwrap();
 }
 
 pub fn prove(input: Input) -> Result<(Vec<Fr>, Proof<Bn254>), Error> {
-    let (cfg,prover_key) = CONFIG
+    let (cfg, prover_key) = CONFIG
         .get()
         .ok_or_else(|| anyhow!("Failed to get circom config"))?;
 
@@ -38,7 +39,7 @@ pub fn prove(input: Input) -> Result<(Vec<Fr>, Proof<Bn254>), Error> {
         .get_public_inputs()
         .ok_or_else(|| anyhow!("Failed to get public inputs"))?;
 
-    let mut rng = thread_rng();
+    let mut rng = ChaChaRng::from_entropy();
     let proof = GrothBn::prove(prover_key, circom, &mut rng)?;
 
     Ok((pi, proof))
