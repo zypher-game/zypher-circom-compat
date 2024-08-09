f5dca478855d48ac5bb1f4e1ede2c3d9ce427de4
Neo Sun <huachuang20@gmail.com>
2024-08-09 21:09:01 +1200

    Add init from bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Error};
 use ark_bn254::{Bn254, Fr};
-use ark_circom::{CircomBuilder, CircomConfig, CircomReduction};
+use ark_circom::{CircomBuilder, CircomConfig, CircomReduction, WitnessCalculator, circom::{R1CS, R1CSFile}};
 use ark_groth16::{Groth16, Proof, ProvingKey};
 use ark_snark::SNARK;
 use num_bigint::BigInt;
@@ -8,6 +8,8 @@ use once_cell::sync::OnceCell;
 use rand::SeedableRng;
 use rand_chacha::ChaChaRng;
 use std::{collections::HashMap, fs::File};
+use wasmer::{Module, Store};
+use std::io::{BufReader, Cursor};
 
 type GrothBn = Groth16<Bn254, CircomReduction>;
 
@@ -22,6 +24,25 @@ pub fn init_config(wasm: &str, r1cs: &str, zkey: &str) {
 
     let mut zkey_file = File::open(zkey).unwrap();
     let (prover_key, _) = ark_circom::read_zkey(&mut zkey_file).unwrap();
+
+    CONFIG.set((cfg, prover_key)).unwrap();
+}
+
+pub fn init_from_bytes(wasm: &[u8], r1cs: &[u8], zkey: &[u8]) {
+    let store = Store::default();
+    let module = Module::new(&store, wasm).unwrap();
+
+    let reader = BufReader::new(Cursor::new(r1cs));
+    let r1cs_file = R1CSFile::<Bn254>::new(reader).unwrap();
+
+    let cfg = CircomConfig {
+        wtns: WitnessCalculator::from_module(module).unwrap(),
+        r1cs: R1CS::from(r1cs_file),
+        sanity_check: false,
+    };
+
+    let mut zkey_reader = BufReader::new(Cursor::new(zkey));
+    let (prover_key, _) = ark_circom::read_zkey(&mut zkey_reader).unwrap();
 
     CONFIG.set((cfg, prover_key)).unwrap();
 }
